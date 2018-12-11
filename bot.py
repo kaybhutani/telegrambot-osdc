040f7338e34f667afc5ce43a93d25e65f7d20f27
Kartikay Bhutani <kbhutani0001@gmail.com>
2018-12-12 02:05:53 +0530

    added top10 feature

--- bot.py
+++ bot.py
@@ -14,12 +14,12 @@ bot = telebot.TeleBot(token=config.bot_token)
 
 
 
-def getRECORD(user_id):
-    records = user_records.find_one({"user_id":user_id})
+def getRECORD(dbname):
+    records = dbname.find({})
     return records
 
-def pushRECORD(record):
-    user_records.insert_one(record)
+def pushRECORD(dbname, record):
+    dbname.insert_one(record)
 
 def updateRecord(record, updates):
     user_records.update_one({'_id': record['_id']},{
@@ -39,9 +39,9 @@ def send_welcome(message):
 def send_welcome(message):
     bot.reply_to(message,'Please send the event details in following format.\n\naddevent\nEvent title\nEvent date(dd/mm/yyyy)\nEvent time\nVenue')
 
-@bot.message_handler(commands=['top10']) # welcome message handler
+@bot.message_handler(commands=['addarticle']) # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message,'Please send article details in following format\ntop10\nArticle title\nURL')
+    bot.reply_to(message,'Please send article details in following format\naddarticle\nArticle title\nURL')
 
 
 @bot.message_handler(func=lambda msg: msg.text is not None and 'addevent' in msg.text.lower())
@@ -54,14 +54,18 @@ def at_converter(message):
 			date=texts[2]
 			time=texts[3]
 			venue=texts[4]
-			format_str = '%d/%m/%Y' # The format
-			date = datetime.datetime.strptime(date, format_str)
-			bot.reply_to(message, date)
-		
-
+			username=message.from_user.username
 			db = client.get_database("jiitosdc")
 			meetings=db.meetings
-
+			rec = {
+			'username':username,
+			'title': title,
+			"date": date,
+			"time": time,
+			"venue": venue,
+			}
+			pushRECORD(meetings, rec)
+			bot.reply_to(message, 'Added event details succesfully!')
 
 
 
@@ -71,6 +75,86 @@ def at_converter(message):
 			print(e)
 	else:
 		bot.reply_to(message, 'Sorry, You don\'t have admin rights')
+
+
+@bot.message_handler(commands=['upcoming']) # welcome message handler
+def send_welcome(message):
+	db = client.get_database("jiitosdc")
+	meetings=db.meetings
+	events=getRECORD(meetings)
+	temp=[]
+	for document in events:
+          temp.append(document)
+	upcoming=[]
+	for i in range (0,len(temp)):
+	    date=temp[i]['date']
+	    format_str = '%d/%m/%Y' # The format
+	    datetime_obj = datetime.datetime.strptime(date, format_str)
+
+	    if(datetime_obj > datetime.datetime.now()):
+	        upcoming.append(temp[i])
+	msg=''
+	for i in range(0,len(upcoming)):
+		msg=msg+'{}\n{}\n{}\n{}\n\n'.format(upcoming[i]['title'],upcoming[i]['date'],upcoming[i]['time'],upcoming[i]['venue'])
+	bot.reply_to(message,msg)
+
+
+
+
+
+@bot.message_handler(func=lambda msg: msg.text is not None and 'addarticle' in msg.text.lower())
+def at_converter(message):
+	admins=['kartikaybhutani']
+	if message.from_user.username in admins:
+		try:
+			texts = message.text.split('\n')
+			title=texts[1]
+			url=texts[2]
+			username=message.from_user.username
+			db = client.get_database("jiitosdc")
+			top10=db.top10
+			rec = {
+			'username':username,
+			'title': title,
+			"url": url,
+			}
+			pushRECORD(top10, rec)
+			bot.reply_to(message, 'Added Article details succesfully!')
+
+
+		except Exception as e:
+			bot.reply_to(message, 'Some error occured. Report to @kartikaybhutani')
+			print(e)
+	else:
+		bot.reply_to(message, 'Sorry, You don\'t have admin rights')
+
+@bot.message_handler(commands=['top10']) # welcome message handler
+def send_welcome(message):
+	db = client.get_database("jiitosdc")
+	top10=db.top10
+	articles=getRECORD(top10)
+	temp=[]
+	for document in articles:
+	      temp.append(document)
+	temp=temp[:10]
+	# upcoming=[]
+	# for i in range (0,len(temp)):
+	#     date=temp[i]['date']
+	#     format_str = '%d/%m/%Y' # The format
+	#     datetime_obj = datetime.datetime.strptime(date, format_str)
+
+	#     if(datetime_obj > datetime.datetime.now()):
+	#         upcoming.append(temp[i])
+	msg=''
+	for i in range(0,len(temp)):
+		msg=msg+'{}\n{}\n\n'.format(temp[i]['title'], temp[i]['url'])
+	bot.reply_to(message,msg)
+
+
+
+
+
+
 
 while True:
     try:
